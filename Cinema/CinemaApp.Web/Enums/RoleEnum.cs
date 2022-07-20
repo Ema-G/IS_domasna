118f39c5524d1bc83c7d47222b202af47be57a3a
Ema-G <ema.georgieva@students.finki.ukim.mk>
2022-07-20 20:47:49 +0200

    promeni

---
+++ Cinema/CinemaApp.Web/Enums/RoleEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Web.Enums
+{
+    public enum RoleEnum
+    {
+        User = 1,
+        Administrator = 2
+    }
+}
